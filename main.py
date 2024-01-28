d82e21563c619b8314eb9ea9057672276f0b3f45
draymone <socotcho@gmail.com>
2024-01-28 21:03:19 +0100

    Clean up code

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pyautogui
-import time
 import keyboard
+import time
 
-while not keyboard.is_pressed('q'):
-    time.sleep(1)
+# Config fields
+SLOT_X = 809
+SLOT_Y = 1011
+SLOT_WIDTH = 40
+HOTKEYS = ["&", "é", "\"", "'", "(", "-", "è", "_", "ç", "à"]
+
+running = True
 
 
 def click(x, y):
@@ -17,54 +22,61 @@ def click(x, y):
     pyautogui.moveTo(x, y)
     # Hold the click for 3 seconds
     pyautogui.mouseDown()
-    time.sleep(3)
+    time.sleep(1.6)
     pyautogui.mouseUp()
 
 
+def on_x_pressed(e):
+    # Stop running the program
+    global running
+    running = False
+    # Unsubscribe
+    keyboard.unhook_all()
+
+
+################
+# MAIN PROGRAM #
+################
+
+# Inform the user
+print("Press X to start the program")
+print("Press X again to stop")
+
+# Wait until the player starts the program
+keyboard.wait("X")
+time.sleep(0.1)
+
+# Subscribe to the on x key pressed event (stops the program)
+keyboard.on_press_key("X", on_x_pressed)
+
+pickaxe_slot = 0
 # Main loop
-while not keyboard.is_pressed('.'):
+while running:
+    # If there is always a pickaxe in the slot
+    if pyautogui.pixel(SLOT_X + pickaxe_slot * SLOT_WIDTH, SLOT_Y) == (135, 102, 39):
+        click(600, 900)
+    else:
+        # Else parse the other slots searching
+        for i in [j for j in range(9) if j != pickaxe_slot]:
+            # When a right slot is found
+            if pyautogui.pixel(SLOT_X + i * SLOT_WIDTH, SLOT_Y) == (135, 102, 39):
+                print(f"Switching to pickaxe {i+1}")
+                # Define it as the actual slot
+                pickaxe_slot = i
+                keyboard.press_and_release(HOTKEYS[i])
 
-    if pyautogui.pixel(799, 1062)[0] == 135:
-        while pyautogui.pixel(799, 1062)[0] == 135:
-            keyboard.press_and_release('1')
-            click(900, 600)
+                click(600, 900)
 
-    if pyautogui.pixel(838, 1062)[0] == 135:
-        while pyautogui.pixel(838, 1062)[0] == 135:
-            keyboard.press_and_release('2')
-            click(900, 600)
+                break
+            else:
+                # Else set temporarily the slot to -1
+                pickaxe_slot = -1
+        # If the slot is always -1
+        if pickaxe_slot == -1:
+            # Stop the program
+            running = False
 
-    if pyautogui.pixel(879, 1062)[0] == 135:
-        while pyautogui.pixel(879, 1062)[0] == 135:
-            keyboard.press_and_release('3')
-            click(900, 600)
-
-    if pyautogui.pixel(919, 1062)[0] == 135:
-        while pyautogui.pixel(919, 1062)[0] == 135:
-            keyboard.press_and_release('4')
-            click(900, 600)
-
-    if pyautogui.pixel(959, 1062)[0] == 135:
-        while pyautogui.pixel(959, 1062)[0] == 135:
-            keyboard.press_and_release('5')
-            click(900, 600)
-
-    if pyautogui.pixel(999, 1062)[0] == 135:
-        while pyautogui.pixel(999, 1062)[0] == 135:
-            keyboard.press_and_release('6')
-            click(900, 600)
-
-    if pyautogui.pixel(1039, 1062)[0] == 135:
-        while pyautogui.pixel(1039, 1062)[0] == 135:
-            keyboard.press_and_release('7')
-            click(900, 600)
-
-    if pyautogui.pixel(1079, 1062)[0] == 135:
-        while pyautogui.pixel(1079, 1062)[0] == 135:
-            keyboard.press_and_release('8')
-            click(900, 600)
-
-    if pyautogui.pixel(1119, 1062)[0] == 135:
-        while pyautogui.pixel(11192, 1062)[0] == 135:
-            keyboard.press_and_release('9')
-            click(900, 600)
+# When the loop execution ends, shut down the program
+print("Shutting down...")
+print("(Press Escape to continue)")
+keyboard.wait('esc')
